e08c301956c990a1cf2a7a479eee7abb5bdc8e34
Nickolas Gupton <nickolas@gupton.xyz>
2024-11-21 23:35:10 -0600

    Update to dotnet 9

--- update-dotnet-tools.sh
+++ update-dotnet-tools.sh
@@ -1,11 +1,21 @@
-cd Frontend/Bones.Api.Client/
+cd Core/Bones.Database
+dotnet tool update dotnet-ef
+dotnet tool restore
+
+cd ../../Frontend/Bones.Api.Client/
 dotnet tool update nswag.consolecore
+dotnet tool restore
+
+cd ../Bones.WebUI/
+sudo dotnet workload restore
 
 cd ../../Services/Bones.Api/
 dotnet tool update swashbuckle.aspnetcore.cli
 dotnet tool update dotnet-rpm
 dotnet tool update dotnet-deb
+dotnet tool restore
 
 cd ../Bones.BackgroundService/
 dotnet tool update dotnet-rpm
 dotnet tool update dotnet-deb
+dotnet tool restore
