8efd71d62fc8c073c2bab6503d4b10b9b3728a53
Nickolas Gupton <nickolas@gupton.xyz>
2025-12-20 00:00:26 -0600

    Update ReQuesty

--- Core/Bones.Api.Client/AutoGen/Models/FieldType.cs
+++ Core/Bones.Api.Client/AutoGen/Models/FieldType.cs
@@ -3,7 +3,7 @@ using System.Runtime.Serialization;
 using System;
 namespace Bones.Api.Client.AutoGen.Models
 {
-    [global::System.CodeDom.Compiler.GeneratedCode("ReQuesty", "0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCode("ReQuesty", "0")]
     #pragma warning disable CS1591
     public enum FieldType
     #pragma warning restore CS1591
