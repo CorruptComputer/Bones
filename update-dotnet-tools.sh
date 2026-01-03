e362c90f1b195ea638a63a0ce9999634186a1163
Nickolas Gupton <nickolas@gupton.xyz>
2026-01-02 19:42:36 -0600

    Remove dotnet-deb and dotnet-rpm, replace with shipyard

--- update-dotnet-tools.sh
+++ update-dotnet-tools.sh
@@ -7,20 +7,9 @@ cd Core/Bones.Database
 dotnet tool update dotnet-ef
 dotnet tool restore
 
-#cd ../../Core/Bones.Api.Client/
-#dotnet tool update nswag.consolecore
-#dotnet tool restore
-
-cd ../Bones.WebUI/
+cd ../../Frontend/Bones.WebUI/
 dotnet workload restore
 
 cd ../../Services/Bones.Api/
 dotnet tool update swashbuckle.aspnetcore.cli
-dotnet tool update dotnet-rpm
-dotnet tool update dotnet-deb
-dotnet tool restore
-
-cd ../Bones.BackgroundService/
-dotnet tool update dotnet-rpm
-dotnet tool update dotnet-deb
 dotnet tool restore
